25be4433e76a782286d0e0df79a33d5106efa7a6
Colton Simpson <rayscolton@gmail.com>
2023-03-10 21:59:16 -0800

    Completed! 3/10/2023

--- src/MyMath.java
+++ src/MyMath.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class MyMath {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int n1 = s.nextInt();
+        System.out.print("Enter second number: ");
+        int n2 = s.nextInt();
+
+        while(n1!=n2)
+        {
+            if(n1>n2)
+                n1=n1-n2;
+            else
+                n2=n2-n1;
+        }
+        System.out.printf("GCF is: " +n2);
     }
 }
